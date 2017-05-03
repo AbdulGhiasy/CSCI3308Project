8850c2d0afe574ed07f2708b58e630e67875be59
Phi Trang <p.trang127@gmail.com>
2017-05-03 16:49:13 -0600

    added more radio buttons

--- ramp.php
+++ ramp.php
@@ -10,53 +10,117 @@ Description: On the CU campus, there are too many obstacles that wheelchair user
 <link rel="icon" href="favicon.png">
 <style>
 body{
-    background-color: #7AA0DF;
+    background-color: #000000;
+    z-index: -2;
+    margin: 0;
+    padding: none;
 }
-p{
+p{  
+  color: white;
     font-weight: bold;
     font-size: 20px;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+p2{
+  color: white;
+    font-weight: bold;
+    font-size: 14px;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+tr{
+  color: white;
+    font-weight: bold;
+    font-size: 20px;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+td{
+  color: white;
+    font-weight: bold;
+    font-size: 20px;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
 }
 h1 {
     font-weight: bold;
     color: black;
-    font-size: 100px;
+    font-size: 90px;
+    padding: 10px;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
 }
-h1 {
+h2 {
     font-weight: bold;
     color: black;
-    font-size: 64px;
+    font-size: 40px;
+    padding: 0px;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    
+}
+div.page-header{
+  
+  background-color: #CFB87C;
+}
+div.bottomContent{
+  background-color: #ffffff;
+  z-index: -1;
+}
+.map{
+
+  background-color: #A2A4A3;
+  width: 570px;
+  padding: 15px;
+  margin: 20px;
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+
+}
+.overview{
+
+  background-color: #A2A4A3;
+  width: 570px;
+  padding: 15px;
+  margin: 20px;
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+
 }
 </style>
+
 <title>
-RampCU
+RampCU - CSCI3308
 </title>
 
 </head>
 <center>
 <div class="page-header">
-  <h1>RampCU</h1>
-  <h2>Map to Success</h2>
+<a href = "https://github.com/AbdulGhiasy/CSCI3308Project.git" target="_blank">
+<h1><u>RampCU</u></h1>
+</a>
+<h2>Map to Success</h2>
 </div>
 
-<a href = "http://www.colorado.edu/" >
+<div id="bottomContent">
+<div class="navColumn"> 
+<div class="overview">
+          Click on the images below to be directed to CU Boulder's website and CU Boulder's Disability Services website. 
+</div>
+
+<a href = "http://www.colorado.edu/" target="_blank">
 <img src="cuboulder.png" style="width=304px;height:228px">
 </a>
-<a href = "http://www.colorado.edu/disabilityservices/">
+<a href = "http://www.colorado.edu/disabilityservices/" target="_blank">
 <img src="handicap.png" style="width=304px;height:228px">
 </a>
 <body>
 
 <div id="bottomContent">
-	<div class="navColumn"> 
-    	<div class="overview">
-        	People restricted to wheelchair accessible travel will be able to use our website to direct them along the best, safe route to their chosen destination on campus. Feel free to use this campus map provided by CU Boulder in order to guide your way around the campus.       </div>
+<div class="navColumn"> 
+<div class="overview">
+People restricted to wheelchair accessible travel will be able to use our website to direct them along the best, safe route to their chosen destination on campus. Feel free to use this campus map provided by CU Boulder in order to guide your way around the campus.       
+</div>
 <p></p>
 <p></p>
 
 <div id="navBottom"></div>
     </div>
-	<div class="contentColumn">
-	</div>
+  <div class="contentColumn">
+  </div>
 
 <iframe src="http://www.colorado.edu/map/?layers=buildings&layers=footpaths#16/40.0090/-105.2686?embed=true"width="600" height="600" scrolling="no" frameborder="0" style="border:0;"></iframe>
 </div>
@@ -64,36 +128,30 @@ RampCU
 
 
 
-
-<!--
-<form action="/ramp.php" method="get">
-<input placeholder="Current Location"type="text" name="location" id="location" required unique><br>
- <input placeholder="End Destination" type="text" name="destination" id="destination" required unique><br>
- <p></p>
-<input type="submit" class="btn btn-success btn-xs">
-<input type="radio" name="location" value="c4c"> c4c<br>
-  <input type="radio" name="destination" value="umc"> umc<br>
-  <input type="submit" value="Submit">
-</form>
--->
-
-<form action="/ramp.php" method="get">
+<form action="/ramp.php" method="get" class="map">
 <h3>Please choose your current building:</h3>
-<input type="radio" name="location" value="c4c" style="vertical-align: middle">C4C<br>
-<input type="radio" name="location" value="engineering" style="vertical-align: middle">Engineering Center<br>
-<input type="radio" name="location" value="rec">CU Recreational Center <br>
-<input type="radio" name="location" value="koelbel">Koelbel<br>
-<input type="radio" name="location" value="umc">UMC<br>
+<input type="radio" name="location" value="c4c" required> C4C<br>
+<input type="radio" name="location" value="engineering"> Engineering Center<br>
+<input type="radio" name="location" value="rec"> CU Recreational Center <br>
+<input type="radio" name="location" value="koelbel"> Koelbel<br>
+<input type="radio" name="location" value="umc"> UMC<br>
+<input type="radio" name="location" value="kittredge"> Kittredge Central<br>
+<input type="radio" name="location" value="muenzinger"> Muenzinger<br>
+<input type="radio" name="location" value="norlin"> Norlin Library<br>
 <h3>Please choose your destination building:</h3>
-<input type="radio" name="destination" value="c4c">C4C<br>
-<input type="radio" name="destination" value="engineering">Engineering Center<br>
-<input type="radio" name="destination" value="rec">CU Recreational Center <br>
-<input type="radio" name="destination" value="koelbel">Koelbel<br>
-<input type="radio" name="destination" value="umc">UMC<br>
+<input type="radio" name="destination" value="c4c" required> C4C<br>
+<input type="radio" name="destination" value="engineering"> Engineering Center<br>
+<input type="radio" name="destination" value="rec"> CU Recreational Center <br>
+<input type="radio" name="destination" value="koelbel"> Koelbel<br>
+<input type="radio" name="destination" value="umc"> UMC<br>
+<input type="radio" name="destination" value="kittredge"> Kittredge Central<br>
+<input type="radio" name="destination" value="muenzinger"> Muenzinger<br>
+<input type="radio" name="destination" value="norlin"> Norlin Library<br>
 <p></p>
-<input type="submit" class="btn btn-success btn-s">
+<input type="submit" class="btn btn-primary btn-s" name = "Submit">
 </form>
-
+<p></p>
+<p></p>
 <?php
 $location = $_GET["location"];
 $destination = $_GET["destination"];
@@ -103,24 +161,26 @@ if(mysqli_connect_errno())
  echo "<h4>Sorry, there is an error in connecting with our database, please contact admins for assistance: phi.trang@colorado.edu, abdul.ghiasy@colorado.edu </h4>".mysqli_connect_error();
 }
 if($destination == $location){
-	echo $location;
-	echo "*Choose a valid location and destination!";
+  echo "<p2>*Choose a valid location and destination!</p2>";
 } else {
 
-
+echo "<p></p>";
+echo "<p></p>";
 $query = "SELECT * FROM $location WHERE destination='$destination'";
 $resultset = mysqli_query($connection, $query);
 
-echo "<table><tr><th>Destination ----> </th><th>Directions</th>";
+echo "<table><tr><th>Destination       &shy; </th><th> Directions</th>";
 while ($row = mysqli_fetch_array($resultset, MYSQLI_NUM)) {
-	print "<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>";
-	$image = $row[2];
+  print "<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>";
+  $image = $row[2];
 }
 echo "</table>";
 echo "<p></p>";
 echo "<p>FOLLOW THE RED PATH</p>";
-
-print "<img src='$image' style='width=1000px;height:1000px'>";
+echo "<p2>The ramps are indicated by the handicap icon</p2>";
+echo "<p></p>";
+echo "<p></p>";
+print "<img src='$image' style='width:750px; height:700px'>";
 }
 
 ?>
